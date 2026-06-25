39059ac9761671b38d0fe946072ea8d2b2659784
Aryan <aryankundalwal1630@gmail.com>
2026-06-25 15:24:30 +0530

    Fix MongoDB connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const cookieParser = require('cookie-parser')
 const path = require("path")
 const {connectTOMongoDB} = require('./connect')
